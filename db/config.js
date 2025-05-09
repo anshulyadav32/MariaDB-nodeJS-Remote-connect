e6573f0af7d3744f20163b7711de24f2bdde1c30
Ravi Kumar <kuravi23er@gmail.com>
2025-05-09 06:47:31 +0530

    Initial commit: Set up MariaDB-NodeJS connection project

--- db/config.js
+++ db/config.js
@@ -1,15 +1,11 @@
-require('dotenv').config();
 const mariadb = require('mariadb');
 
-// Create a connection pool
 const pool = mariadb.createPool({
   host: process.env.DB_HOST, // Remote MariaDB host
   user: process.env.DB_USER, // Database username
   password: process.env.DB_PASSWORD, // Database password
   database: process.env.DB_NAME, // Database name
-  port: process.env.DB_PORT, // Add port
-  connectionLimit: 10, // Increase pool size
-  acquireTimeout: 30000, // Increase timeout to 30 seconds
+  connectionLimit: 5, // Connection pool limit
   ssl: false, // Disable SSL
 });
 
